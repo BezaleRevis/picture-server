f880895ded4991fc26b7b02ba551826bdb65e68b
BezaleRevis <bezalel803@gmail.com>
2022-06-25 14:48:09 +0300

    after hecoru

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(bodyParser.json());
