7abb9c33f5b93796df8ee2730e4ffb86dfe94866
BezaleRevis <bezalel803@gmail.com>
2022-06-25 15:49:00 +0300

    data base

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 let con = mysql.createConnection({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+  host: "us-cdbr-east-05.cleardb.net" || process.env.database,
+  user: "bc15022fc4dc2c" || process.env.user,
+  password: "44b69c46" || process.env.password,
+  database: "heroku_f4d1fd157ccd4f3" || process.env.database,
 });
 function createServerSql() {
   return new Promise((resolve, reject) => {
@@ -20,4 +20,6 @@ function createServerSql() {
     resolve();
   });
 }
+
 module.exports = { con,createServerSql };
+// mysql://bc15022fc4dc2c:44b69c46@us-cdbr-east-05.cleardb.net/heroku_f4d1fd157ccd4f3?reconnect=true
