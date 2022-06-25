5b1295b10d7ac71aca50626b109e179038b68a95
BezaleRevis <bezalel803@gmail.com>
2022-06-25 16:34:50 +0300

    eh

--- app.js
+++ app.js
@@ -35,19 +35,19 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello tzali pictures project");
 });
 
-async function startServer() {
-  // setting up the server
-  if (cluster.isMaster) {
-    console.log("Master has been started...");
-    const NUM_WORKERS = os.cpus().length;
-    for (let i = 0; i < NUM_WORKERS; i++) {
-      cluster.fork();
-    }
-  } else {
-    console.log("problem with sql");
-  }
-}
-startServer();
+// async function startServer() {
+//   // setting up the server
+//   if (cluster.isMaster) {
+//     console.log("Master has been started...");
+//     const NUM_WORKERS = os.cpus().length;
+//     for (let i = 0; i < NUM_WORKERS; i++) {
+//       cluster.fork();
+//     }
+//   } else {
+//     console.log("problem with sql");
+//   }
+// }
+// startServer();
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
